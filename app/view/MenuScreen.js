07c4836c9c3506eb37a343eaa9a07ee91daeed9f
nikos@intrasoft <rizopoulos@isa0676.athens.intrasoft-intl.private>
2014-07-23 17:15:50 +0300

    drawer fixed. initial menu screen added

---
+++ app/view/MenuScreen.js
@@ -0,0 +1,17 @@
+Ext.define('nonq_userapp.view.MenuScreen', {
+	extend: 'nonq_userapp.ux.Drawer',
+	xtype: 'menuscreen',
+	config:{
+    	direction: 'horizontal',
+		defaultActive: 'drawer',
+	    openDrawerLength: 0.1,
+		backgroundItem:{
+	        html : 'menu',
+	        style: 'background-color: red'
+		},
+		drawerItem:{
+            html : 'Drawer',
+            style: 'background-color: grey'
+		}
+	}
+});

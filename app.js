365246858a7314fbed0d4802bf690e2fd7a3dcf6
nikos@intrasoft <rizopoulos@isa0676.athens.intrasoft-intl.private>
2014-08-27 17:21:02 +0300

    Test Store Picker screen

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ Ext.application({
     ],
 
     views: [
-        'Test'
+        'TestStorePickerScreen'
     ],
 
     icon: {
@@ -44,7 +44,7 @@ Ext.application({
         Ext.fly('appLoadingIndicator').destroy();
 
         // Initialize the main view
-        Ext.Viewport.add(Ext.create('nonq_userapp.view.Test'));
+        Ext.Viewport.add(Ext.create('nonq_userapp.view.TestStorePickerScreen'));
     },
 
     onUpdated: function() {
--- app/view/TestStorePickerScreen.js
+++ app/view/TestStorePickerScreen.js
@@ -1,7 +1,7 @@
-Ext.define('nonq_userapp.view.Test', {
+Ext.define('nonq_userapp.view.TestStorePickerScreen', {
 	extend: 'Ext.Container',
 	requires: ['nonq_userapp.view.Header','nonq_userapp.view.Footer','nonq_userapp.view.StorePickerWrapper'],
-	xtype: 'testview',
+	xtype: 'teststorepickerscreen',
 	config:{
 		layout: 'vbox',
 		items:[
